946e99741661d6722660d14cc45ad26d9e5a32a7
vaaadik22 <vaaadik22@gmail.com>
2022-02-23 20:49:01 +0300

    feat:add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let stack = [];
+  bracketsConfig.forEach(bracket => {
+    stack.push(bracket.(''));
+  })
+  
+let i = 0;
+while(i < stack.length){
+  if(str.includes(stack[i])){
+    str.str.replace(stack[i], '');
+    i = 0;
+  } else{
+    i++
+  }
+}
+return str.length === 0;
 }
